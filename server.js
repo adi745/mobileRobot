14898fe657b5098c3f571dafdb1698609d4b09e9
adi <adi745@users.noreply.github.com>
2024-05-10 12:25:47 +0300

    now i'm able to operate right and left motors in front

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { Board, Led, Servo, Proximity, Motor, Motors } = require("johnny-five");
 const express = require("express");
+const cors = require("cors");
 
 const board = new Board();
 const opn = require("opn");
@@ -10,48 +11,57 @@ const startUrl = `http://localhost:${port}`;
 const boardPort = "COM6";
 
 //pin setup
-const pwmFront = 9;
+const pwmFrontR = 9;
 const cdirFrontRightMotor = 22;
 const dirFrontRightMotor = 24;
 const cdirFrontLeftMotor = 26;
 const dirFrontLeftMotor = 28;
+const pwmFrontL = 10;
 
-const pwmBack = 11;
+const pwmBackR = 11;
 const cdirBackRightMotor = 5;
 const dirBackRightMotor = 6;
 const cdirBackLeftMotor = 7;
 const dirBackLeftMotor = 8;
+const pwmBackL = 12;
 
 // opn(`${startUrl}`);
+app.use(cors());
 
 board.on("ready", () => {
   console.log("board is ready....");
-  const servo = new Servo({ pin: 13, startAt: 90, range: [45, 135], fps: 6 });
+  //   const servo = new Servo({ pin: 13, startAt: 90, range: [45, 135], fps: 6 });
   const motorsF = new Motors([
     {
+      //right motor
       pins: {
-        pwm: pwmFront,
+        pwm: pwmFrontR,
         dir: dirFrontRightMotor,
         cdir: cdirFrontRightMotor,
       },
       invertPWM: false,
     },
     {
-      pins: { pwm: pwmFront, dir: dirFrontLeftMotor, cdir: cdirFrontLeftMotor },
+      //left motor
+      pins: {
+        pwm: pwmFrontL,
+        dir: dirFrontLeftMotor,
+        cdir: cdirFrontLeftMotor,
+      },
       invertPWM: false,
     },
   ]);
   const motorsB = new Motors([
     {
       pins: {
-        pwm: pwmBack,
+        pwm: pwmBackR,
         dir: dirBackRightMotor,
         cdir: cdirBackRightMotor,
       },
       invertPWM: false,
     },
     {
-      pins: { pwm: pwmBack, dir: dirBackLeftMotor, cdir: cdirBackLeftMotor },
+      pins: { pwm: pwmBackL, dir: dirBackLeftMotor, cdir: cdirBackLeftMotor },
       invertPWM: false,
     },
   ]);
@@ -74,26 +84,38 @@ board.on("ready", () => {
   //     pin: 30,
   //   });
 
-  //   board.repl.inject({
-  //     servo,
-  //     // frMotor,
-  //     // flMotor,
-  //     servo,
-  //     motorsF,
-  //     motorsB,
-  //   });
+  board.repl.inject({
+    // servo,
+    // frMotor,
+    // flMotor,
+    motorsF,
+    motorsB,
+  });
 
   app.get("/leftFrontOn", (req, res) => {
     res.send("LEFT MOTOR ON!");
-    motorsF[0].forward(200);
+    motorsF[1].forward(200);
     res.sendStatus(200);
   });
 
   app.get("/leftFrontStop", (req, res) => {
     res.send("LEFT MOTOR STOP!");
+    motorsF[1].stop();
+    res.sendStatus(200);
+  });
+
+  app.get("/rightFrontOn", (req, res) => {
+    res.send("RIGHT MOTOR ON!");
+    motorsF[0].forward(200);
+    res.sendStatus(200);
+  });
+
+  app.get("/rightFrontStop", (req, res) => {
+    res.send("RIGHT MOTOR STOP!");
     motorsF[0].stop();
     res.sendStatus(200);
   });
+
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
